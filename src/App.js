de3aa23645efba02de840d76303b61b9a5022b07
Tom Hart <kitsonian@gmail.com>
2022-05-23 13:30:01 -0400

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './css/app.css';
+
+const initialState = [
+  {
+    'done': false,
+    'label': 'Pay electiric bill'
+  },
+  {
+    'done': false,
+    'label': 'Take Lady for a walk'
+  },
+  {
+    'done': false,
+    'label': 'Water cuttings'
+  },
+]
 
 function App() {
+
+  const [list, setList] = useState(initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex justify-center items-center h-screen bg-slate-900 text-cyan-50'>
+      <div className='pl-8 border-l-2 border-l-cyan-500'>
+        <h3 className='bg-slate-900 -ml-64'>Monday, May 23, 2022</h3>
+        <ul>
+          {list.map(item =>
+            <li key={item.label}>{item.label}</li>
+          )}
+        </ul>
+        <footer className='mt-6'>
+          <button className='border-2 border-cyan-500 rounded px-4 py-2'>Add new item</button>
+        </footer>
+      </div>
     </div>
   );
 }
