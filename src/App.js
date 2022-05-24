8f74c54d0bbcde8087c032b369dd33d8386432f3
Tom Hart <kitsonian@gmail.com>
2022-05-24 16:22:00 -0400

    Preserve pencil emoji

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
                   ? <input type='text' onBlur={() => setEditItemId(false)} onChange={e => dispatch({ type: ACTION.EDITITEM, id, newLabel: e.target.value })} className='bg-transparent border-2 border-cyan-500 px-4 py-2 rounded block' />
                   : <span onClick={() => dispatch({ type: ACTION.TOGGLEDONE, id: id })} className={`${done ? 'line-through text-slate-600' : ''} ml-4 w-60`}>{label}</span>
                 }
-                <span onClick={() => setEditItemId(id)} className='ml-4 opacity-50 hover:opacity-100'>{!done && <span className=''>✏</span>}</span>
+                <span onClick={() => setEditItemId(id)} className='ml-4 opacity-50 hover:opacity-100'>{!done && <span className='font-sans'>✏</span>}</span>
               </li>
             )}
           </ul>
