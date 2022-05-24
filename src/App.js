cf10b328b75d1f187ace10ab83456e914c262b20
Tom Hart <kitsonian@gmail.com>
2022-05-24 09:56:59 -0400

    Adding new item

--- src/App.js
+++ src/App.js
@@ -1,40 +1,90 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import './css/app.css';
+
+const ACTION = {
+  'TOGGLEDONE': 'toggleDone',
+  'ADDNEWITEM': 'addNewItem'
+}
 
 const initialState = [
   {
-    'done': false,
-    'label': 'Pay electiric bill'
+    id: 1,
+    done: false,
+    label: 'Pay electiric bill'
   },
   {
-    'done': false,
-    'label': 'Take Lady for a walk'
+    id: 2,
+    done: true,
+    label: 'Take Lady for a walk'
   },
   {
-    'done': false,
-    'label': 'Water cuttings'
+    id: 3,
+    done: false,
+    label: 'Water cuttings'
   },
 ]
 
+const reducer = (state, action) => {
+
+  const { id } = action;
+
+  switch (action.type) {
+
+    case 'toggleDone':
+      return [...state.map(item => item.id === id ? { ...item, done: !item.done } : item)];
+
+    case 'addNewItem':
+      return [...state, { id: state.length + 1, done: false, label: action.label }]
+
+    default:
+      return [...state];
+  }
+}
+
 function App() {
 
-  const [list, setList] = useState(initialState);
+  const [list, dispatch] = useReducer(reducer, initialState);
+  const [newItem, setNewItem] = useState(false);
+  const [newItemText, setNewItemText] = useState('');
+
+  const handleNewItem = (label) => {
+    dispatch({ type: ACTION.ADDNEWITEM, label })
+    setNewItem(false);
+    setNewItemText('');
+  }
 
   return (
-    <div className='flex justify-center items-center h-screen bg-slate-900 text-cyan-50'>
-      <div className='pl-8 border-l-2 border-l-cyan-500'>
-        <h3 className='bg-slate-900 -ml-64'>Monday, May 23, 2022</h3>
-        <ul>
-          {list.map(item =>
-            <li key={item.label}>{item.label}</li>
-          )}
-        </ul>
-        <footer className='mt-6'>
-          <button className='border-2 border-cyan-500 rounded px-4 py-2'>Add new item</button>
-        </footer>
+    <>
+      <div className='flex justify-center items-center h-screen bg-slate-900 text-cyan-50'>
+        <div className='p-8 border-l-2 border-l-cyan-500'>
+          <h3 className='sm:-ml-64'>Monday, May 23, 2022</h3>
+          <ul>
+            {list.map((item, i) =>
+              <li key={i} className='cursor-pointer hover:bg-red-500' onClick={() => dispatch({ type: ACTION.TOGGLEDONE, id: item.id })}>
+                {item.done ? <span>✔</span> : <span className='opacity-10'>✔</span>}
+                <span className={item.done ? 'line-through text-slate-600' : ''}>{item.label}</span>
+              </li>
+            )}
+          </ul>
+          <footer className='mt-6'>
+            {
+              newItem
+                ? <>
+                  <input onChange={e => setNewItemText(e.target.value)} value={newItemText} type='text' className='bg-transparent border-2 border-cyan-500 px-4 py-2 rounded block' />
+                  <button onClick={() => handleNewItem(newItemText)} className='border-2 bg-cyan-500 border-cyan-500 text-cyan-900 rounded px-4 py-2 block'>Add new item</button>
+                </>
+
+                : <button onClick={() => setNewItem(true)} className='border-2 border-cyan-500 rounded px-4 py-2'>Add new item</button>
+            }
+          </footer>
+        </div>
       </div>
-    </div>
+      <pre>{JSON.stringify(list, null, 2)}</pre>
+    </>
   );
 }
 
 export default App;
+
+const getIndexOfItem = (state, index) => state.findIndex(item => item.id === index);
+const getSingleItemById = (state, id) => state.filter(item => item.id === id)[0]
