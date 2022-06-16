42eb53cd03463b7f4c9f3863820706579da7ced4
Tom Hart <kitsonian@gmail.com>
2022-06-16 15:07:32 -0400

    Updating tests

--- src/initialState.js
+++ src/initialState.js
@@ -2,16 +2,16 @@ const today = new Date();
 const yesterday = new Date(Date.now() - 24 * 3600 * 1000);
 const initialState = [
     {
-        id: 1,
-        date: yesterday,
-        done: false,
-        label: 'Pay electiric bill'
-    },
-    {
         id: 2,
         date: today,
         done: true,
         label: 'Take Lady for a walk'
+    },
+    {
+        id: 1,
+        date: yesterday,
+        done: false,
+        label: 'Pay electiric bill'
     },
     {
         id: 3,
